768f93d124aa7a64a54ec3bb36f000c1fa1a8390
Danielle Smith <dsmith18@sjastudents.org>
2020-09-16 04:05:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    System.out.println("Enter a number to count down or up from");
+        for (int i=0; i < 10; i++)
+    {
+    System.out.println("i is " + i);
+    }
+    
+    System.out.println("enter");
+      for (int count = 10; count >= 0; count --)
+      {
+        System.out.println(count);
+      }
+
+    System.out.println("while");
+      int c = 0;
+      while (c < 7)
+      {
+        System.out.println(c);
+        c += 2;
+      }
+      
+    switch (number) {
+    default:
+    System.out.println(number + "Blast off!");
+    }
+  }
+}
